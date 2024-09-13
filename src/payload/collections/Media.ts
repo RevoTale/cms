8b9738ecb1ba1c2f1b888540fac319eaaf26b7a5
Liakhovskyi Vladyslav <rusted@revotale.com>
2024-09-14 01:19:13 +0300

    fix nextjs build env variable missing

--- src/payload/collections/Media.ts
+++ src/payload/collections/Media.ts
@@ -39,26 +39,23 @@ export const Media: CollectionConfig = {
     },
   ],
   upload: {
+
     adminThumbnail: 'card',
     imageSizes: [
       {
         name: 'card',
         width: 640,
-        height: 480,
       },
       {
         name: 'portrait',
         width: 768,
-        height: 1024,
       },
       {
         name: 'square',
         width: 1200,
-        height: 1200,
       },
       {
         name: 'feature',
-        width: 1024,
         height: 576,
       },
     ],

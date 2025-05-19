6b30e3c75a7e4aaa9330ef730d00828266361c51
l-you <l-you@revotale.com>
2025-05-19 22:32:12 +0000

    Make a good UI for auto translation

--- src/app/components/AutoTranslation/AutoTranslateButton.tsx
+++ src/app/components/AutoTranslation/AutoTranslateButton.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Button, useConfig, useDocumentInfo, useLocale } from "@payloadcms/ui";
+import { Button, useConfig, useDocumentInfo } from "@payloadcms/ui";
 import { TypedLocale } from "payload";
 import { FunctionComponent, useState, useTransition } from "react";
 import { autoTranslate } from "./autoTranslate";
@@ -8,7 +8,6 @@ import { autoTranslate } from "./autoTranslate";
 const AutoTranslateButton:FunctionComponent= () => {
   const { id,collectionSlug, } = useDocumentInfo()
  const {config:{localization}}= useConfig()
-const targetLocale = useLocale().code
 
   const [isPending, startTransition]  = useTransition()
 const [pendingLocale,setPendingLocale] = useState<string|null>(null)
@@ -21,9 +20,11 @@ const [results,setResults] = useState<{
     return null
   }
   const sourceLocale = localization.defaultLocale
+  const locales = localization.locales.map(locale => locale.code).filter(locale => locale !== sourceLocale)
   const handleSubmit = ()=>{
     startTransition(async ()=>{
-      try {
+      for (const targetLocale of locales) {
+        try {
         setPendingLocale(targetLocale)
         const result = await autoTranslate({
             docId: id.toString(),
@@ -40,10 +41,11 @@ const [results,setResults] = useState<{
       } catch (error:unknown) {
          setResults(prev=>[...prev,{locale:targetLocale,error:error instanceof Error ? error.message : 'Unknown error'}])
       }
+      }
     })
   }
     return <div>
-        <Button onClick={handleSubmit} disabled={isPending} type="submit">{isPending?`Translating to ${pendingLocale}...`:`Auto Translate from ${sourceLocale}`}</Button>
+        <Button onClick={handleSubmit} disabled={isPending} type="submit">{isPending?`Translating to ${pendingLocale}...`:`Auto Translate All from ${sourceLocale}`}</Button>
         <div>
             {results.map((result,index)=><div key={index} className="flex gap-2">
                 <div>{result.locale}: </div>{result.error ? <div style={{color:'red'}}>{result.error}</div>:<div style={{color:'green'}}>translated</div>}

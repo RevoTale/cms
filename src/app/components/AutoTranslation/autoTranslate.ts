46bd0abb115aff52377bb7cc43d39330f264fcf1
l-you <l-you@revotale.com>
2025-05-19 23:34:21 +0000

    Finally, managed to make translation of nested fields working!!!!!!!

--- src/app/components/AutoTranslation/autoTranslate.ts
+++ src/app/components/AutoTranslation/autoTranslate.ts
@@ -30,22 +30,23 @@ export const autoTranslate = async ({docId,collection,targetLocale,sourceLocale}
     const dataToUpdate :RecursivePartial<typeof post>= {
         
     }
-console.log('post',post)
 
     const iterateOverFields = async(fields:Field[],obj:DataFromCollectionSlug<typeof collection>,data:Record<string,unknown>)=>{
         for (const [key, value] of Object.entries(obj)) {
+
             for (const field of fields) {
-            
                 if ((field.type === 'text' || field.type === 'textarea') && field.localized === true && field.name === key && typeof value === 'string') {
                         data[key  ] = await translateFn(value,targetLocale)
                 } else if(field.type === 'tabs') {
                     for (const tab of field.tabs) {
-                        
                         if ( ('name' in tab && tab.name === key) ) {
                              const subObj = obj[key as keyof DataFromCollectionSlug<typeof collection>]
                         if (typeof subObj === 'object' && !Array.isArray(subObj) && subObj !== null) {
                             data[key] = data[key] ?? {}
-                          iterateOverFields(tab.fields, subObj, data[key] as Record<string,unknown>)
+                            await iterateOverFields(tab.fields, subObj, data[key] as Record<string,unknown>)
+                          if (Object.keys(data[key] as Record<string,unknown>).length === 0) {
+                            delete data[key]    
+                          }
                         }
                         }
                        
@@ -55,8 +56,7 @@ console.log('post',post)
             }
         }
     }
-console.log('data',dataToUpdate)
-    //iterateOverFields(fields,post ,dataToUpdate)
+    await iterateOverFields(fields,post ,dataToUpdate)
     // Update the document with the translated data
     if (Object.entries(dataToUpdate).length === 0) {
        return {

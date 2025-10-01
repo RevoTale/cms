4f76ca1167b0aec7e178d4191be8b7972a03640c
l-you <l-you@revotale.com>
2025-10-01 10:08:36 +0000

    migration savepoint

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250929_225423 from './20250929_225423';
+import * as migration_20251001_075702 from './20251001_075702';
 
 export const migrations = [
   {
     up: migration_20250929_225423.up,
     down: migration_20250929_225423.down,
-    name: '20250929_225423'
+    name: '20250929_225423',
+  },
+  {
+    up: migration_20251001_075702.up,
+    down: migration_20251001_075702.down,
+    name: '20251001_075702'
   },
 ];

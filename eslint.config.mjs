feb97c12e01c1fae2c54b760e9553f3113e08a5a
l-you <l-you@revotale.com>
2025-10-05 11:16:53 +0000

    Ignore some eslint rules to make migration to the new niuce enxtjs cofnig smoother

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,33 @@ const eslintConfig = defineConfig(
 			'.next/**',
 			'next-env.d.ts','eslint.config.mjs']),
 			niceNextjs,
+			{
+				rules: {
+"@typescript-eslint/no-unsafe-assignment": 'off',
+"@typescript-eslint/strict-boolean-expressions": 'off',
+"@typescript-eslint/prefer-nullish-coalescing": 'off',
+"@typescript-eslint/no-unsafe-member-access": 'off',
+"@typescript-eslint/no-unsafe-return": 'off',
+"@typescript-eslint/no-unsafe-type-assertion":"off",
+"@typescript-eslint/prefer-destructuring": "off",
+"@typescript-eslint/prefer-optional-chain": "off",
+
+      // New global relaxations
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/init-declarations': 'off',
+      '@typescript-eslint/consistent-type-assertions': 'off',
+      '@typescript-eslint/no-unnecessary-type-parameters': 'off',
+      '@typescript-eslint/no-unnecessary-type-constraint': 'off',
+      'eslint-comments/require-description': 'off',
+      'eslint-comments/disable-enable-pair': 'off',
+
+
+      'logical-assignment-operators': 'off',
+
+				}
+			},
 	prettier,
 ]
 )

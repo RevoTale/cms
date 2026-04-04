b2f405f0e76a15dcedfa77af8ec24f1371de80bb
l-you <l-you@revotale.com>
2026-04-05 02:21:04 +0300

    fix: intropsection didsabling

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -208,7 +208,7 @@ const toTagId = (tag: string | Tag): string => (typeof tag === 'string' ? tag : 
 export default buildConfig({
 	defaultDepth: payloadDefaultDepth,
 	graphQL: {
-		disableIntrospectionInProduction: process.env.ENABLE_GQL_INTROPSECTION === '1',
+		disableIntrospectionInProduction: process.env.ENABLE_GQL_INTROPSECTION !== '1',
 		disablePlaygroundInProduction: true,
 		maxComplexity: payloadGraphQLMaxComplexity,
 		schemaOutputFile: path.resolve(dirname, './gql/schema.graphql'),
